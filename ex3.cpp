e87583c8436c4fcb8899eb29b2f6b228e9d5abe0
Mariusz Perzynski <m.perzyk@interia.pl>
2022-06-16 21:24:14 +0200

    first push

---
+++ ex3.cpp
@@ -0,0 +1,13 @@
+// Korzystając z programu opisanego w zadaniu 1  - Lista laboratoryjna nr 2, 
+// stwórz tablicę obiektów klasy StudentEdukacja.
+// Posortuj tablicę według numeru indeksu studenta wykorzystując mechanizm klasyzaprzyjaźnionej(dodatkowa klasa odpowiedzialna za sortowanie).
+// W programie zastosuj podejście obiektowe pokazane na poniższym diagramie UML (klasa Student Edukacja dziedziczy składniki po klasie Student)
+
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+#include <ctime>
+#include <dos.h>
