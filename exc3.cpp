2642a887b2d95b78b416248329829ca98e6827e1
Mariusz Perzynski <m.perzyk@interia.pl>
2022-06-17 10:30:09 +0200

    commit rewolt

--- exc3.cpp
+++ exc3.cpp
@@ -1,3 +1,4 @@
+// zmodyfikowana czesc zadania z listy 3, do latwiejszego wprowadzenia w liste 4 
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,18 +11,21 @@ using namespace std;
 
 class Student
 {
-    string myName, myStudentID, myClassID;
+    string myName, myStudentID, myClassID, myAgeID, myGroupID;
+
 public:
-    Student(string name, string studentID, string classID)
+    Student(string name, string studentID, string classID, string ageID, string groupID)
     {
         myName = name;
         myStudentID = studentID;
         myClassID = classID;
+        myAgeID = ageID;
+        myGroupID = groupID;
     }
 
     string getName() { return myName; }
     string getStudentID() { return myStudentID; }
-    void printInfo() { cout << myName << setw(25) << myStudentID << setw(20) << myClassID << endl; }
+    void printInfo() { cout << myName << setw(25) << myStudentID << setw(20) << myClassID << setw(20) << myAgeID << setw(20) << myGroupID << endl; }
 
 };
 
@@ -29,6 +33,7 @@ int main()
 {
     int num;
     std::vector<Student> studentList;
+    
     cout << "Ilu studentow dodac do listy? " << endl;
     cin >> num;
 
@@ -37,7 +42,7 @@ int main()
     for (int a = 0; a < num; a++)
     {
 
-        string inputName, inputStudentID, inputClassID;
+        string inputName, inputStudentID, inputClassID, inputAgeID, inputGroupID;
 
         cout << "Podaj imie: ";
         getline(cin, inputName);
@@ -50,9 +55,15 @@ int main()
         cout << "Podaj numer katalogu studenta: ";
         getline(cin, inputClassID);
 
+        cout << "Podaj wiek studenta: ";
+        getline(cin, inputAgeID);
 
 
-        Student thisStudent(inputName, inputStudentID, inputClassID);
+        cout << "Podaj grupe studenta: ";
+        getline(cin, inputGroupID);
+
+
+        Student thisStudent(inputName, inputStudentID, inputClassID, inputAgeID, inputGroupID);
         studentList.push_back(thisStudent);
 
         cout << endl;
@@ -65,7 +76,7 @@ int main()
     cout << endl;; 
 
 
-    cout << "IMIE STUDENTA" << setw(25) << "NAZWISKO STUDENTA" << setw(20) << "INDEX STUDENTA" << endl;
+    cout << "IMIE STUDENTA" << setw(25) << "NAZWISKO STUDENTA" << setw(20) << "INDEX STUDENTA" << setw(20) << "WIEK STUDENTA" << setw(20) << "GRUPA STUDENTA" << endl;
     for (int a = 0; a < studentList.size(); a++)
     {
         studentList[a].printInfo();
